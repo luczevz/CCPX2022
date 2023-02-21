aba1019dca20fc35f0a65604764c534be09c0287
Lucas Azevedo <lukas537@hotmail.com>
2023-02-21 17:17:53 -0300

    projeto CCPX2022 com countDown

---
+++ hour.js
@@ -0,0 +1,66 @@
+/* const a1 = document.getElementById("a1");
+const a2 = document.getElementById("a2");
+const a3 = document.getElementById("a3");
+const a4 = document.getElementById("a4");
+
+const datalancamento = "21 January 2024";
+
+function countDown(){
+    const datalance = new Date(datalancamento);
+    const hoje = new Date();
+
+    const segTotal = (datalance - hoje) / 1000;
+
+    const dias = Math.floor(segTotal / 3600 / 24);
+    const horas = Math.floor((segTotal / 3600) % 24);
+    const minutos = Math.floor(segTotal / 60) % 60;
+    const segundos = Math.floor(segTotal % 60);
+
+    
+
+    a1.innerHTML = `<strong>${dias}D</strong>`;
+    a2.innerHTML = `<strong>${horas}H</strong>`;
+    a3.innerHTML = `<strong>${minutos}M</strong>`;
+    a4.innerHTML = `<strong>${segundos}S</strong>`;
+}
+
+countDown();
+
+setInterval(countDown, 1000); */
+
+
+const a1 = document.getElementById("a1");
+const a2 = document.getElementById("a2");
+const a3 = document.getElementById("a3");
+const a4 = document.getElementById("a4");
+
+const datalancamento = "21 January 2024";
+
+function countDown(){
+
+    const datalance = new Date(datalancamento);
+    const hoje = new Date();
+
+
+
+    const segTotal = (datalance - hoje)/1000;
+
+    const dias = Math.floor(segTotal / 3600 / 24);
+    const horas = Math.floor((segTotal/3600) % 24);
+    const minutos = Math.floor(segTotal / 60) % 60;
+    const segundos = Math.floor(segTotal % 60);
+
+    a1.innerHTML = ` <strong>${dias}D</strong>`;
+    a2.innerHTML = ` <strong>${horas}H</strong>`;
+    a3.innerHTML = ` <strong>${minutos}M</strong>`;
+    a4.innerHTML = ` <strong>${segundos}S</strong>`;
+}
+
+countDown();
+
+setInterval(countDown, 1000);
+
+
+
+
+
---
+++ readme.md.txt
@@ -0,0 +1,9 @@
+Opáaa bem vindo
+
+Este projeto foi realizando utilizando HTML, CSS e JavaScript para countDown, o contador de tempo no caso até a data de evento que esta até 2024
+
+Creio que ficou alinhado de acordo com a resolução e paramentros, mas de qualquer forma, da pra melhorar com certeza.
+
+é isso. 
+Até a proxima e me segue.
+
